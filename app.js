f811c03dc3e138d7df0d16a95e63f48d0fd45459
Kaleb Murphy <kalebmurphy@gmail.com>
2013-09-27 14:17:33 -0500

    Configuring new project

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kalebmurphy on 9/27/13.
+ */
+
+
+var express = require("express");
+var app = express();
+
+
+app.use(express.static(__dirname + '/public', {maxAge: 1}));
+
+app.listen(3000);
+
