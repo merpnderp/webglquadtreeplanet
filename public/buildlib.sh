3371042dd3f38154ab97dbfad35f06ed74b03a41
merpnderp <kalebmurphy@gmail.com>
2014-01-21 15:05:48 -0600

    updated buidlib to build everything.

--- public/buildlib.sh
+++ public/buildlib.sh
@@ -1,1 +1,2 @@
-browserify -r ./libs/three.js -r ./libs/stats.js > ./libs.js
+browserify -r ./libs/three.js -r ./libs/stats.js > ./libs.js;
+browserify -v -t brfs -x ./libs/three.js -x ./libs/stats.js app/main.js -o ./app.js;
