fa5dbc4dc1ece2c2330463ebd123388090ee4a8e
merpnderp <kalebmurphy@gmail.com>
2014-02-06 17:23:45 -0600

    Added webworker test

---
+++ public/webworkertest/build.sh
@@ -0,0 +1,1 @@
+watchify -v -t brfs -x ../libs/three.js -x ../libs/stats.js main.js -o ./app.js
--- public/webworkertest/main.js
+++ public/webworkertest/main.js
@@ -2,66 +2,83 @@
  * Created by kalebmurphy on 2/1/14.
  */
 
+var pause = false;
 
-var THREE = require('../libs/three.js');
-
-//var workerObject = require('./worker.js');
-
-var main = function () {
-
-    var camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 7000000);
-    camera.position.z = 10;
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-    var scene = new THREE.Scene();
-
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('viewport').appendChild(renderer.domElement);
-
-    var pause = false;
-
-    var worker = new Worker('webworkertest/worker.js');
-
-    var average = 0, attempts=0;
-
-    worker.addEventListener('message', function(e) {
-        console.log(e.data);
-        var array = new Float64Array(e.data.typedArray.buffer);
-        console.log('array length: ' + array.length);
-        console.log("10th: " + array[10]);
-        var time = new Date().getTime();
-        average += (time - e.data.time);
-        attempts++;
-//        console.log( average + " : " + attempts);
-        console.log("average time to pass: " + (average / attempts));
-        worker.postMessage({msg: 'go'});
-    }, false);
-
-    worker.postMessage({msg: 'go'});
-
-    function render() {
-
-
+window.addEventListener("keypress", function (key) {
+    if (key.key === "Spacebar") {
+        pause = !pause;
         if(!pause){
-//            requestAnimationFrame(render);
-        }else{
-            setTimeout(render(),1);
+            worker.postMessage({msg: 'go'});
         }
-
-        renderer.render(scene, camera);
-
     }
 
-    render();
+});
 
-    window.addEventListener("keypress", function (key) {
-        if (key.key === "Spacebar") {
-            pause = !pause;
-        }
+var worker = new Worker('webworkertest/worker.js');
 
-    });
+var average = 0, attempts = 0, creationTotal = 0, bestGen = 10000, worstGen = 0, bestReturn = 10000, worstReturn = 0 ;
+
+var canvas = document.getElementById('c'),
+    ctx = canvas.getContext('2d'),
+    imgdata = ctx.getImageData(0, 0, canvas.width, canvas.height),
+    data = imgdata.data,
+    t = 0;
+
+var delta = new Date().getTime() - 2000;
+console.log(imgdata.data.length);
+worker.addEventListener('message', function (e) {
+
+    var time = new Date().getTime();
+
+    var returnTime = time - e.data.time;
+
+    if(returnTime < bestReturn){
+        bestReturn = returnTime;
+    }else if(returnTime > worstReturn){
+        worstReturn = returnTime;
+    }
+
+    var creation = e.data.creation;
+
+    if(creation < bestGen){
+        bestGen = creation;
+    }else if(creation > worstGen){
+        worstGen = creation;
+    }
 
 
-};
-window.addEventListener("load", main);
+    average += returnTime;
+    creationTotal += creation;
+    attempts++;
+
+    var array = new Uint8ClampedArray(e.data.typedArray.buffer);
+
+    imgdata.data.set(array);
+
+    ctx.putImageData(imgdata, 0, 0);
+
+//        console.log( average + " : " + attempts);
+
+    if(new Date().getTime() > delta + 1000){
+        updateLogs();
+        delta = new Date().getTime();
+    }
+
+    if(!pause){
+        worker.postMessage({msg: 'go'});
+    }
+}, false);
+
+function updateLogs(){
+    document.getElementById('text').innerHTML = ""+
+        "Best Gen: " + bestGen + "<br/>"+
+        "Worst Gen: " + worstGen + "<br/>"+
+        "average time to build array: " + (creationTotal / attempts)  + "<br/>" +
+        "Best Return: " + bestReturn + "<br/>"+
+        "Worst Return: " + worstReturn + "<br/>"+
+        "average time to return from worker: " + (average/attempts) + "<br/>";
+
+}
+
+worker.postMessage({msg: 'go'});
+
